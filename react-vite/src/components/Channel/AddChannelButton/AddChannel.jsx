e567c10f3e0102303844966ecf1d9c6721a8bde2
GaimerGai <devinmrichards95@gmail.com>
2024-02-17 14:26:28 -0700

    Pre-Debug of Channel update

--- react-vite/src/components/Channel/AddChannelButton/AddChannel.jsx
+++ react-vite/src/components/Channel/AddChannelButton/AddChannel.jsx
@@ -1,18 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useModal } from '../../../context/Modal';
 import ChannelCreateModal from '../ChannelCreateModal/ChannelCreateModal';
 import OpenModalMenuItem from '../../Navigation/OpenModalMenuItem';
 
 const AddChannelButton = () => {
-  const { showModal } = useModal();
+  const ulRef = useRef(null);
+  const [showMenu, setShowMenu] = useState(false);
+  const closeMenu = (e) => {
+    if (!ulRef.current?.contains(e.target)) {
+      setShowMenu(false);
+    }
+  };
 
   return (
     <div className='serverTile group relative overflow-hidden'>
       <button
         className="flex items-center justify-center h-12 w-12 rounded-full hover:bg-gray-700 transition-all duration-300 ease-in-out"
-        onClick={() => showModal(<ChannelCreateModal />)}
+        onClick={() => setShowMenu(true)}
       >
-        +
+        <OpenModalMenuItem
+          className="text-green-500 text-2xl"
+          itemText="+"
+          onItemClick={closeMenu}
+          modalComponent={<ChannelCreateModal/>}
+        />
       </button>
     </div>
   );
--- react-vite/src/components/Servers/ServerSettingButton/ServerSettingButton.jsx
+++ react-vite/src/components/Servers/ServerSettingButton/ServerSettingButton.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { thunkFetchServerById } from '../../../redux/server';
-
 import OpenModalMenuItem from '../../Navigation/OpenModalMenuItem';
 import ServerSettingModal from '../ServerSettingModal/ServerSettingModal';
 
